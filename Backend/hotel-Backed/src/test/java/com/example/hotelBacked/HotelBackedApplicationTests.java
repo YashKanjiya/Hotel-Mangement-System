0e432bf54115ea4148a377f3ab1b250627390809
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-04-06 22:19:39 +0530

    first commit

---
+++ Backend/hotel-Backed/src/test/java/com/example/hotelBacked/HotelBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelBacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
